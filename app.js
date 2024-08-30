7b173b0e103351ce2dcfb540d3fb364a7279bf6e
Keaton Whitehead <keaton@blueprint-health.com>
2024-08-30 09:51:57 -0400

    example of auth and selectClient

--- app.js
+++ app.js
@@ -1,15 +1,19 @@
+require('dotenv').config()
 const express = require('express');
 const path = require('path');
+const fetch = require('node-fetch');
 
 const app = express();
 const port = 3333;
 
 // Sample patient data
 const patients = [
-  { id: 1, name: 'John Doe', age: 35, condition: 'Flu' },
-  { id: 2, name: 'Jane Smith', age: 28, condition: 'Asthma' },
-  { id: 3, name: 'Michael Johnson', age: 42, condition: 'Diabetes' }
+  { id: process.env.BLUEPRINT_CLIENT_ID, name: 'John Doe', age: 35, condition: 'Flu' },
+  { id: process.env.BLUEPRINT_CLIENT_ID, name: 'Jane Smith', age: 28, condition: 'Asthma' },
+  { id: process.env.BLUEPRINT_CLIENT_ID, name: 'Michael Johnson', age: 42, condition: 'Diabetes' }
 ];
+
+const clinicianId = process.env.BLUEPRINT_CLINICIAN_ID;
 
 // Middleware to serve static files
 app.use(express.static(path.join(__dirname, 'public')));
@@ -45,25 +49,60 @@ app.get('/patients', (req, res) => {
 });
 
 // Route for individual patient profiles
-app.get('/patients/:id', (req, res) => {
-  const patientId = parseInt(req.params.id);
+app.get('/patients/:id', async (req, res) => {
+  const patientId = req.params.id;
   const patient = patients.find(p => p.id === patientId);
+
+  // Retrieve an api access token from Blueprint
+  const tokenResponse = await fetch(
+    `${process.env.BLUEPRINT_API_URL}/partners/authenticate`,
+    {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json',
+        'X-Api-Key': `${process.env.BLUEPRINT_API_KEY}`,
+      },
+      body: JSON.stringify({
+        clientId: process.env.BLUEPRINT_CLIENT_ID,
+        clientSecret: process.env.BLUEPRINT_CLIENT_SECRET,
+      }),
+    }
+  )
+
+  const { accessToken } = await tokenResponse.json();
+
+  // authenticate the clinician with Blueprint
+  const authResponse = await fetch(
+    `${process.env.BLUEPRINT_API_URL}/clinicians/${clinicianId}/authenticate`,
+    {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json',
+        'Access-Token': accessToken,
+        'X-Api-Key': `${process.env.BLUEPRINT_API_KEY}`,
+      },
+      body: JSON.stringify({ email: 'clinician+email@email.com' }),
+    }
+  )
+
+  const clinicianTokens = await authResponse.json();
 
   if (patient) {
       res.send(`
-          <script>
-            window.blueprintSettings = {
-              containerId: 'blueprint-container'
-            }
-          </script>
-          <script src="https://embed.stagingprint.com/index.min.js"></script>
-          <script>Blueprint.selectClient(${patient.id})</script>
-          <h2>Patient Profile</h2>
-          <p><strong>Name:</strong> ${patient.name}</p>
-          <p><strong>Age:</strong> ${patient.age}</p>
-          <p><strong>Condition:</strong> ${patient.condition}</p>
-          <a href="/patients">Back to Patient List</a>
-          <div id="blueprint-container"></div>
+        <script>
+          window.blueprintSettings = {
+            containerId: 'blueprint-container'
+          }
+        </script>
+        <script src="https://embed.stagingprint.com/index.min.js"></script>
+        <script>Blueprint.authenticate(${JSON.stringify(clinicianTokens)}, ${JSON.stringify(clinicianId)})</script>
+        <script>Blueprint.selectClient(${JSON.stringify(patient.id)})</script>
+        <h2>Patient Profile</h2>
+        <p><strong>Name:</strong> ${patient.name}</p>
+        <p><strong>Age:</strong> ${patient.age}</p>
+        <p><strong>Condition:</strong> ${patient.condition}</p>
+        <a href="/patients">Back to Patient List</a>
+        <div id="blueprint-container"></div>
       `);
   } else {
       res.status(404).send('Patient not found');

cf16e335c3fca08b834a2a2b45517367355f838f
Keaton Whitehead <keaton@blueprint-health.com>
2024-09-04 11:11:48 -0400

    body parser.json

--- app.js
+++ app.js
@@ -8,8 +8,6 @@ const fetch = require('node-fetch');
 const app = express();
 const port = process.env.PORT || 3000;
 
-app.use(express.json())
-
 app.use(express.static(path.join(__dirname, 'public')));
 app.use(express.urlencoded({ extended: true }));
 app.use(bodyParser.json());

861e606d4ee89b45438ab2320cf45d65e43ad008
Jim Fiorato <jfiorato@gmail.com>
2025-11-25 14:39:51 -0600

    Debugging

--- .ecs_deploy/docker-entrypoint.sh
+++ .ecs_deploy/docker-entrypoint.sh
@@ -6,6 +6,8 @@ ENV_FILE_NAME=".env-cmdrc.json"
 rm -f $ENV_FILE_NAME
 aws secretsmanager get-secret-value --region us-west-2 --secret-id $AWS_SECRET_MANAGER_ARN --query SecretString --output text > $ENV_FILE_NAME
 
+echo "Downloading patient data from: s3://$SAMPLE_FILE_BUCKET/patients.$ENVIRONMENT.json"
+
 aws s3 cp "s3://$SAMPLE_FILE_BUCKET/patients.$ENVIRONMENT.json" "data/patients.$ENVIRONMENT.json"
 
 exec "$@"

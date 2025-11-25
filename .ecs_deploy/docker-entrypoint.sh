0e61c135f5ef2b12030a5f318e43a7b52322237d
Jim Fiorato <jfiorato@gmail.com>
2025-11-25 14:28:49 -0600

    Copy sample data

--- .ecs_deploy/docker-entrypoint.sh
+++ .ecs_deploy/docker-entrypoint.sh
@@ -6,4 +6,6 @@ ENV_FILE_NAME=".env-cmdrc.json"
 rm -f $ENV_FILE_NAME
 aws secretsmanager get-secret-value --region us-west-2 --secret-id $AWS_SECRET_MANAGER_ARN --query SecretString --output text > $ENV_FILE_NAME
 
+aws s3 cp "s3://$SAMPLE_FILE_BUCKET/patients.$ENVIRONMENT.json" "data/patients.$ENVIRONMENT.json"
+
 exec "$@"
